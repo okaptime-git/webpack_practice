b584d91a51e786c6ce3c61377ea65e0298fb1b9f
okap <44427502+okaptime-git@users.noreply.github.com>
2024-02-14 09:09:44 +0900

    extract css and html

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,6 @@
 const path = require('path');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   entry: './src/index.js',
@@ -6,19 +8,28 @@ module.exports = {
     path: path.resolve(__dirname, './dist'),
     filename: 'main.js',
   },
+  // output
   module: {
     rules: [
       {
         test: /\.css/,
         use: [
           {
-            loader: 'style-loader', // style-loaderを適用してね（下から上に適用される）<style></style>として、jsを通して出力される
+            // loader: 'style-loader', // style-loaderを適用してね（下から上に適用される）<style></style>として、jsを通して出力されるが、別のものを使用する！
+            loader: MiniCssExtractPlugin.loader,
           },
           {
-            loader: 'css-loader', // cssで見つけたら、
+            loader: 'css-loader', // cssを見つけたら読み込んで、、、
           },
         ],
       },
     ],
   },
+  // module
+  plugins: [
+    new MiniCssExtractPlugin(),
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+    }),
+  ],
 };
